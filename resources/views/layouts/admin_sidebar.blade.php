10ceb31a418f627f100c8973203b4fba47ad8fa6
Jean Urena <juanpedro1519@gmail.com>
2017-04-01 00:45:20 -0400

    Some Stilying to programs Frontend

--- resources/views/layouts/admin_sidebar.blade.php
+++ resources/views/layouts/admin_sidebar.blade.php
@@ -31,19 +31,19 @@
     <ul class="sidebar-menu">
       <li class="header">HEADER</li>
       <!-- Optionally, you can add icons to the links -->
-      <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
-      <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
       <li class="treeview">
-        <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
+        <a href="#"><i class="fa fa-link"></i> <span>Tables</span>
           <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
           </span>
         </a>
         <ul class="treeview-menu">
-          <li><a href="#">Link in level 2</a></li>
+          <li><a href="/admin/programs">Programs</a></li>
           <li><a href="#">Link in level 2</a></li>
         </ul>
       </li>
+      <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
+      <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
     </ul>
     <!-- /.sidebar-menu -->
   </section>
--- resources/views/programs/show.blade.php
+++ resources/views/programs/show.blade.php
@@ -2,17 +2,58 @@
 
 @section('title') {{ $program-> name }} @endsection
 @section('content')
-    <!-- Projects Row -->
+    <!-- Portfolio Item Heading -->
     <div class="row">
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="http://placehold.it/700x400" alt="">
-            </a>
-            <h3>
-                <a href="#">{{ $program->name }}</a>
-            </h3>
-            <p>{{ $program->description }}</p>
+        <div class="col-lg-12">
+            <h1 class="page-header text-center">{{ $program->name }}</h1>
+            <h3 class="text-center">{{ $program->slogan }}</h3>
         </div>
     </div>
+    <!-- /.row -->
+    <hr>
+    <!-- Portfolio Item Row -->
+    <div class="row">
+        <div class="col-md-6">
+            <p>{{ $program->description }}</p>
+        </div>
+        <div class="col-md-6">
+            <img class="img-responsive" src="http://placehold.it/750x500" alt="">
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <!-- Related Projects Row -->
+    <div class="row">
+
+        <div class="col-lg-12">
+            <h3 class="page-header">Featured Courses</h3>
+        </div>
+
+        <div class="col-sm-3 col-xs-6">
+            <a href="#">
+                <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+            </a>
+        </div>
+
+        <div class="col-sm-3 col-xs-6">
+            <a href="#">
+                <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+            </a>
+        </div>
+
+        <div class="col-sm-3 col-xs-6">
+            <a href="#">
+                <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+            </a>
+        </div>
+
+        <div class="col-sm-3 col-xs-6">
+            <a href="#">
+                <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+            </a>
+        </div>
+
+    </div>
+    <!-- /.row -->
     <!-- /.row -->
 @endsection
