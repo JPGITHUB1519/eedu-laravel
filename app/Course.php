dd71689c588e0d776e214afb969188a88368aa9c
Jean Urena <juanpedro1519@gmail.com>
2017-04-26 20:25:20 -0400

    Add Many to Many Relationship to Program and Course Models

--- app/Course.php
+++ app/Course.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+	public $table = "programs";
+    // The Programs that the course belongs
+    public function programs()
+    {
+    	return $this->belongsToMany('App\Program', 'programs_vs_courses');
+    }
 }
--- app/Program.php
+++ app/Program.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Program extends Model
 {
-    //
+	public $table = "programs";
+    // The courses of the programs
+    public function courses()
+    {
+    	// params @model, @table
+    	return $this->belongsToMany('App\Course', 'programs_vs_courses');
+    }
 }
