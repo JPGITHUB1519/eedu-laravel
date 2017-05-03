a4819f02a45449c676fb5e15a1e717189010a11e
Jean Urena <juanpedro1519@gmail.com>
2017-05-02 20:57:49 -0400

    Add Bootstrap cdn until solve the problem

--- resources/views/landing_index.blade.php
+++ resources/views/landing_index.blade.php
@@ -10,9 +10,10 @@
 
     <title>Creative - Start Bootstrap Theme</title>
     <!-- Bootstrap Core CSS, this is not include because laravel already come with bootstrap -->
-    {{--  <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css', config('app.use_ssl')) }}" rel="stylesheet"> --}}
+    {{-- <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css', config('app.use_ssl')) }}" rel="stylesheet"> --}}
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <!-- app.css laravel generated css file -->
-    <link href="{{ asset('css/app.css', config('app.use_ssl')) }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/app.css', config('app.use_ssl')) }}" rel="stylesheet"> --}}
     <!-- Custom Fonts -->
     <link href="{{ asset('vendor/font-awesome/css/font-awesome.min.css', config('app.use_ssl')) }}" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
